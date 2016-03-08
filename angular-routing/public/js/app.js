f87c563857d6fc0190a7e5252bea2f346256683c
Taylor Huston <taylor.a.huston@gmail.com>
2016-03-07 22:43:38 -0700

    Set up some basic controllers

---
+++ angular-routing/public/js/app.js
@@ -0,0 +1,35 @@
+angular.module('routerApp', [])
+
+//Main site controller
+.controller('mainController', function () {
+
+  var vm = this;
+  vm.bigMessage = "Hello";
+
+})
+
+//Home page controller
+.controller('homeController', function () {
+
+  var vm = this;
+  vm.message = "Home page";
+  
+})
+
+
+//About page controller
+.controller('aboutController', function () {
+
+  var vm = this;
+  vm.message = "About page";
+
+})
+
+//Contact page controller
+.controller('contactController', function() {
+  
+  var vm = this;
+  vm.message = "Contact";
+  
+});
+
--- angular-routing/server.js
+++ angular-routing/server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/public'));
 
 //Set up route for index file
 app.get('*', function(req, res) {
-  res.sendFile(path,joise(__dirname + '/public/views/index/html'));
+  res.sendFile(path.join(__dirname + '/public/views/index.html'));
 });
 
 //Start server
