a03888f0a548e94c9b6729e9081bb307ee7b9481
Taylor Huston <taylor.a.huston@gmail.com>
2016-03-09 22:46:42 -0700

    Setup config.js

---
+++ user-crm/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'port' : 'prcoess.env.PORT' || 8080,
+  'database' : 'mongodb://taylor:testpassword@ds059125.mlab.com:59125/taylor-meanmachine',
+  'secret' : 'ilovescotchscotchyscotchscotch'
+};
