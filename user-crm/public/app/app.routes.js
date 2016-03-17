49a26244c4aab66481a135dcdfd1bc58566a843d
Taylor Huston <taylor.a.huston@gmail.com>
2016-03-16 20:51:20 -0700

    Add login route

--- user-crm/public/app/app.routes.js
+++ user-crm/public/app/app.routes.js
@@ -3,10 +3,18 @@ angular.module('app.routes', ['ngRoute'])
 .config(function($routeProvider, $locationProvider) {
   
   $routeProvider
+  
   //Home page
   .when('/', {
     templateUrl : 'app/views/pages/home.html'
   });
+  
+  //Login page
+  .when'/login', {
+    templateUrl : 'app/views/pages/login.html',
+    controller : 'mainController',
+    controllerAs: 'login'
+  }
   
   $locationProvider.html5Mode(true);
   
